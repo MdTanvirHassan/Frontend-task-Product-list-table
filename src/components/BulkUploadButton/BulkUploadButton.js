59ffecf2eb1f7be377c7d5362684d24a196a5f6b
MdTanvirHassan <103600961+MdTanvirHassan@users.noreply.github.com>
2023-01-16 12:27:36 +0600

    1 file modified

--- src/components/BulkUploadButton/BulkUploadButton.js
+++ src/components/BulkUploadButton/BulkUploadButton.js
@@ -146,7 +146,7 @@ const ProductList = () => {
                     <input
                       type="text"
                       value={product.price}
-                      className="text-center relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800  border-2 border-gray-500"
+                      className="text-center relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-blue dark:text-blue focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800  border-2 border-gray-500 absolute"
                       onChange={(e) => handlePriceChange(e, index)}
                     />
                   ) : (
